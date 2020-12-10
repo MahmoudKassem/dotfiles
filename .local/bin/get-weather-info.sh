8396a8cb722358e102965bb71feaef91d342f3ba
Mahmoud Kassem <mail@itqan.de>
2020-12-10 18:16:47 +0100

    fix regex

--- .local/bin/get-weather-info.sh
+++ .local/bin/get-weather-info.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 weatherInfo=$(curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs)
-weatherCondition=$(printf "%s" "$weatherInfo" | sed -r 's/([^0-9]+)([0-9]+.*)/\1/' | xargs)
-temperature=$(printf "%s" "$weatherInfo" | sed -r 's/([^0-9]+)([0-9]+.*)/\2/')
+weatherCondition=$(printf "%s" "$weatherInfo" | sed -r 's/([^0-9|\-]+)(\-?[0-9]+.*)/\1/' | xargs)
+temperature=$(printf "%s" "$weatherInfo" | sed -r 's/([^0-9|\-]+)(\-?[0-9]+.*)/\2/')
 case $weatherCondition in
   "Clear") icon= ;;
   "Cloudy") icon=摒 ;;
