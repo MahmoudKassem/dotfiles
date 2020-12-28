917cb0c560bd6c3484ea2d2cba60f31ae949c4e9
Mahmoud Kassem <mail@itqan.de>
2020-12-28 23:21:11 +0100

    add weather condition Patchy moderate snow

--- .local/bin/get-weather-info.sh
+++ .local/bin/get-weather-info.sh
@@ -24,6 +24,7 @@ case $weatherCondition in
   "Overcast") icon=摒 ;;
   "Partly cloudy") icon=  ;;
   "Patches of fog") icon=  ;;
+  "Patchy moderate snow") icon=ﭽ  ;;
   "Patchy rain possible") icon=  ;;
   "Sunny") icon=  ;;
   "Thundery heavy rain") icon=  ;;
