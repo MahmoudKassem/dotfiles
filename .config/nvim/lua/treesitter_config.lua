746aeb93a5e1f6f94b88b337abc69ba69aca5109
Mahmoud Kassem <mail@itqan.de>
2021-01-23 08:56:16 +0100

    use the indent module

--- .config/nvim/lua/treesitter_config.lua
+++ .config/nvim/lua/treesitter_config.lua
@@ -1,4 +1,5 @@
 require('nvim-treesitter.configs').setup {
   highlight = { enable = true },
-  incremental_selection = { enable = true }
+  incremental_selection = { enable = true },
+  indent = { enable = true }
 }
