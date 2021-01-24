25da1730c252aeab0f94bd4a24fdd84e93037280
Mahmoud Kassem <mail@itqan.de>
2021-01-24 19:08:42 +0100

    install lua language server via AUR

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -23,13 +23,9 @@ lsp_config.vimls.setup {
 }
 
 --sumneko for lua
-local language_server_path = os.getenv("XDG_CACHE_HOME") .. "/nvim/nvim_lsp/sumneko_lua"
 lsp_config.sumneko_lua.setup {
   on_attach = on_attach,
-  cmd = {
-    string.format("%s/lua-language-server/bin/Linux/lua-language-server", language_server_path),
-    "-E", string.format("%s/sumneko_lua/lu--language-server/main.lua", language_server_path)
-  }
+  cmd = { "lua-language-server", "-E", "/usr/local/share/lua-language-server/main.lua" }
 }
 
 --configure diagnostics
