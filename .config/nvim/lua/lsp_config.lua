32a7f9f0c985841e8769cc13597c6642b6ccbf80
Mahmoud Kassem <mail@itqan.de>
2021-01-24 09:05:05 +0100

    enable updating during insert mode

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -37,7 +37,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     signs = false,
     underline = true,
-    update_in_insert = false,
+    update_in_insert = true,
     virtual_text = {
       spacing = 2,
       prefix = ''
