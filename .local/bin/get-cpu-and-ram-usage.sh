95acb1a7755b0cdcd591cbfe47c919dcdff34fd2
Mahmoud Kassem <mail@itqan.de>
2021-03-01 21:45:48 +0100

    change icon for ram usage

--- .local/bin/get-cpu-and-ram-usage.sh
+++ .local/bin/get-cpu-and-ram-usage.sh
@@ -4,4 +4,4 @@ systemInfo=$(top -b -i -n 1)
 cpuUsage=$(printf "%s" "$systemInfo" | awk '/Cpu/ {print $2;}')
 ramUsage=$(printf "%s" "$systemInfo" | awk '/MiB Mem/ {printf "%.1f", ($8 / $4 * 100);}')
 
-printf "   %s%%  %s%% \\n" "$cpuUsage" "$ramUsage"
+printf "   %s%%   %s%% \\n" "$cpuUsage" "$ramUsage"
