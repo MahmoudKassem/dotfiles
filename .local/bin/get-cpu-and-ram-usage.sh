385bc768af0cce4cea98a0bfb0de99af8c2113cd
Mahmoud Kassem <mail@itqan.de>
2021-03-02 10:15:21 +0100

    round cpu and ram usage

--- .local/bin/get-cpu-and-ram-usage.sh
+++ .local/bin/get-cpu-and-ram-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 systemInfo=$(top -b -i -n 1)
-cpuUsage=$(printf "%s" "$systemInfo" | awk '/Cpu/ {print $2;}')
-ramUsage=$(printf "%s" "$systemInfo" | awk '/MiB Mem/ {printf "%.1f", ($8 / $4 * 100);}')
+cpuUsage=$(printf "%s" "$systemInfo" | awk '/Cpu/ {printf "%.0f", $2;}')
+ramUsage=$(printf "%s" "$systemInfo" | awk '/MiB Mem/ {printf "%.0f", ($8 / $4 * 100);}')
 
 printf "   %s%%   %s%% \\n" "$cpuUsage" "$ramUsage"
