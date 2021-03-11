b6b36b61c45b4a07ec75b3bfc490e68451b9633d
Mahmoud Kassem <mail@itqan.de>
2021-03-11 20:41:30 +0100

    add more keybindings for telescope builtins

--- .config/nvim/lua/pluginConfig.lua
+++ .config/nvim/lua/pluginConfig.lua
@@ -1,24 +1,7 @@
-local command = vim.api.nvim_command
 local globalVariables = vim.g
 
 --completion-nvim
 globalVariables.completion_matching_strategy_list = {"exact", "substring", "fuzzy"}
-
---netrw
-globalVariables.netrw_banner = 0
-globalVariables.netrw_browse_split = 4
-globalVariables.netrw_bufsettings = 'nobuflisted number relativenumber'
-globalVariables.netrw_liststyle = 3
-globalVariables.netrw_preview = 1
-globalVariables.netrw_winsize = 25
-command([[
-function ToggleNetrw()
-  if exists("g:netrw_buffer") && bufexists(g:netrw_buffer)
-    exe "bwipeout" . g:netrw_buffer | unlet g:netrw_buffer
-  else
-    Vexplore | let g:netrw_buffer=bufnr("%")
-  endif
-endfunction ]])
 
 --vim-airline
 globalVariables.airline_powerline_fonts = 1
