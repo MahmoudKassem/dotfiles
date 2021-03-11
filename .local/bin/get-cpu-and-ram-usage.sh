b7e4dbd5ca02d6cae2c552cc03386ecd1abf6158
Mahmoud Kassem <mail@itqan.de>
2021-03-11 09:53:25 +0100

    determine cpu usage more accurately

--- .local/bin/get-cpu-and-ram-usage.sh
+++ .local/bin/get-cpu-and-ram-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 systemInfo=$(top -b -i -n 1)
-cpuUsage=$(printf "%s" "$systemInfo" | awk '/Cpu/ {printf "%.0f", $2;}')
+cpuUsage=$(printf "%s" "$systemInfo" | awk '/Cpu/ {printf "%.0f", (100 - $8);}')
 ramUsage=$(printf "%s" "$systemInfo" | awk '/MiB Mem/ {printf "%.0f", ($8 / $4 * 100);}')
 
 printf "   %s%%   %s%% \\n" "$cpuUsage" "$ramUsage"
