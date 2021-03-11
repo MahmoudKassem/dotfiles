9de4789321bf5851055878e87096fd69089ac07c
Mahmoud Kassem <mail@itqan.de>
2021-03-11 21:48:32 +0100

    add vim rooter patterns for go modules and stack projects

--- .config/nvim/lua/pluginConfig.lua
+++ .config/nvim/lua/pluginConfig.lua
@@ -10,6 +10,6 @@ globalVariables['airline#extensions#tabline#enabled'] = 1
 
 --vim-rooter
 globalVariables.rooter_change_directory_for_non_project_files = 'current'
-globalVariables.rooter_patterns = {".git"}
+globalVariables.rooter_patterns = {".git", "*.mod", "stack*.yaml"}
 globalVariables.rooter_resolve_links = 1
 globalVariables.rooter_silent_chdir = 1
