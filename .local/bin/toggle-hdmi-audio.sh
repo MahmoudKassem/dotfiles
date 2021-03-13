31977a1c8c63768fe37ce96912bb65832b662b1b
Mahmoud Kassem <mail@itqan.de>
2021-03-13 13:25:35 +0100

    exit earlier when .asoundrc file already exists

--- .local/bin/toggle-hdmi-audio.sh
+++ .local/bin/toggle-hdmi-audio.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]
   else user=$1
 fi
 
-hdmiPcm=$(aplay --list-devices | sed -n -E 's/^card ([0-9]+).*device ([0-9]+).*HDMI.*$/pcm.!default "hw:\1,\2"/p')
 alsaConfigPath="/home/$user/.asoundrc"
 [ -f "$alsaConfigPath" ] && rm "$alsaConfigPath" && exit
+hdmiPcm=$(aplay --list-devices | sed -n -E 's/^card ([0-9]+).*device ([0-9]+).*HDMI.*$/pcm.!default "hw:\1,\2"/p')
 printf "%s" "$hdmiPcm" > "$alsaConfigPath" && chown "$user" "$alsaConfigPath" && chmod 644 "$alsaConfigPath"
