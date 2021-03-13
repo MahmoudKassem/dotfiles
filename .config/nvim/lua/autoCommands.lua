dad816e6b35ebdf35321883b23de4bbd54bfc5e9
Mahmoud Kassem <mail@itqan.de>
2021-03-13 22:59:16 +0100

    delete unused autoCommands

--- .config/nvim/lua/autoCommands.lua
+++ .config/nvim/lua/autoCommands.lua
@@ -3,8 +3,5 @@ local function autoCommand(command)
 end
 
 autoCommand('BufNewFile,BufRead *.pro set filetype=prolog')
-autoCommand('FileType qf nnoremap <buffer> <cr> <cr>:cclose<cr>')
-autoCommand('FileType qf nnoremap <s-k> :cnext<cr>:wincmd j<cr>')
-autoCommand('FileType qf nnoremap <s-j> :cprev<cr>:wincmd j<cr>')
 autoCommand('TermOpen * startinsert')
 autoCommand('TextYankPost * silent! lua vim.highlight.on_yank()')
