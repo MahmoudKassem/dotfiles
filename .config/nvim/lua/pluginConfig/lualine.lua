dd66a86c5fd4d9adb3476dd4afa897ba06f1f10b
Mahmoud Kassem <mail@itqan.de>
2021-03-15 15:06:05 +0100

    options of lualine already match the default + add config for inactive_sections

--- .config/nvim/lua/pluginConfig/lualine.lua
+++ .config/nvim/lua/pluginConfig/lualine.lua
@@ -1,10 +1,4 @@
 require('lualine').status {
-  options = {
-    theme = 'gruvbox',
-    section_separators = {'', ''},
-    component_separators = {'', ''},
-    icons_enabled = true,
-  },
   sections = {
     lualine_a = { {'mode', upper = true} },
     lualine_b = { {'branch', icon = ''}, 'diff' },
@@ -12,5 +6,13 @@ require('lualine').status {
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location'  },
-  }
+  },
+  inactive_sections = {
+    lualine_a = { {'branch', icon = ''}, 'diff' },
+    lualine_b = { {'filename', file_status = true} },
+    lualine_c = {  },
+    lualine_x = {  },
+    lualine_y = {  },
+    lualine_z = {  },
+  },
 }
