fa91456252177f7dfd69c01d189a7fb59a99345b
Mahmoud Kassem <mail@itqan.de>
2021-03-16 08:09:24 +0100

    fix weather condition to icon mapping

--- .local/bin/get-weather-info.sh
+++ .local/bin/get-weather-info.sh
@@ -10,10 +10,10 @@ case $weatherCondition in
   "Heavy rain" | "Heavy showers") icon= ;;
   "Heavy snow" | "Heavy snow showers" | "Patchy heavy snow") icon= ;;
   "Light drizzle" | "Light rain" | "Light rain shower" | "Light showers" | "Moderate rain" | "Patchy light drizzle" | \
+  "Patchy light rain" | "Patchy rain possible") icon= ;;
   "Light sleet" | "Light sleet showers" | "Moderate snow" | "Patchy moderate snow" | "Light snow" | \
   "Light snow showers") icon= ;;
   "Partly cloudy") icon= ;;
-  "Patchy light rain" | "Patchy rain possible") icon= ;;
   "Thundery heavy rain" | "Thundery outbreaks possible") icon= ;;
   "Thundery showers" | "Thundery snow showers") icon= ;;
   *) icon="???";;
