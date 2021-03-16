055e1573794294cf90c6bfa9ff9b4aef806a564b
Mahmoud Kassem <mail@itqan.de>
2021-03-16 20:55:57 +0100

    put diff before branch

--- .config/nvim/lua/pluginConfig/lualine.lua
+++ .config/nvim/lua/pluginConfig/lualine.lua
@@ -1,11 +1,11 @@
 require('lualine').status {
   sections = {
     lualine_a = { {'mode', upper = true} },
-    lualine_b = { {'branch', icon = ''}, 'diff' },
+    lualine_b = { 'diff', {'branch', icon = ''} },
     lualine_c = { {'filename', file_status = true} },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
-    lualine_z = { 'location'  },
+    lualine_z = { 'location' },
   },
   inactive_sections = {
     lualine_a = { {'filename', file_status = true} },
