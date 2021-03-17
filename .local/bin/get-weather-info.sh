b8aba1b2ca94accf3f317fd0391cc00548cb26fa
Mahmoud Kassem <mail@itqan.de>
2021-03-17 20:58:34 +0100

    add Patchy sleet possible weather condition

--- .local/bin/get-weather-info.sh
+++ .local/bin/get-weather-info.sh
@@ -9,10 +9,10 @@ case $weatherCondition in
   "Fog" | "Mist" | "Patches of fog") icon= ;;
   "Heavy rain" | "Heavy showers") icon= ;;
   "Heavy snow" | "Heavy snow showers" | "Patchy heavy snow") icon= ;;
-  "Light drizzle" | "Light rain" | "Light rain shower" | "Light showers" | "Moderate rain" | "Patchy light drizzle" | \
-    "Patchy light rain" | "Patchy rain possible") icon= ;;
-  "Light sleet" | "Light sleet showers" | "Moderate snow" | "Patchy moderate snow" | "Light snow" | \
-    "Light snow showers") icon= ;;
+  "Light drizzle" | "Light rain" | "Light rain shower" | "Light showers" | "Moderate rain" | \
+    "Patchy light drizzle" | "Patchy light rain" | "Patchy rain possible") icon= ;;
+  "Light sleet" | "Light sleet showers" | "Light snow" |  "Light snow showers" | "Moderate snow" | \
+    "Patchy moderate snow" | "Patchy sleet possible" ) icon= ;;
   "Partly cloudy") icon= ;;
   "Thundery heavy rain" | "Thundery outbreaks possible") icon= ;;
   "Thundery showers" | "Thundery snow showers") icon= ;;
