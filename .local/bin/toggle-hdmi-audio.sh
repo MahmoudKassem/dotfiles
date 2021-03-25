bd066fac98197eec1f90419a56ac94d8fef36a5a
Mahmoud Kassem <mail@itqan.de>
2021-03-25 20:18:33 +0100

    set users' primary group for the .asoundrc file

--- .local/bin/toggle-hdmi-audio.sh
+++ .local/bin/toggle-hdmi-audio.sh
@@ -6,6 +6,8 @@ if [ $# -lt 1 ]
 fi
 
 alsaConfigPath="/home/$user/.asoundrc"
+primaryGroup=$(id -gn "$user")
 [ -f "$alsaConfigPath" ] && rm "$alsaConfigPath" && exit
 hdmiPcm=$(aplay --list-devices | sed -n -E 's/^card ([0-9]+).*device ([0-9]+).*HDMI.*$/pcm.!default "hw:\1,\2"/p')
-printf "%s" "$hdmiPcm" > "$alsaConfigPath" && chown "$user" "$alsaConfigPath" && chmod 644 "$alsaConfigPath"
+printf "%s" "$hdmiPcm" > "$alsaConfigPath" \
+  && chown "$user" "$alsaConfigPath" && chmod 644 "$alsaConfigPath" && chgrp "$primaryGroup" "$alsaConfigPath"
