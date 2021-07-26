3692f02e8c6bf15f36bea8d5dcd450f08e8d50cc
Mahmoud Kassem <mail@itqan.de>
2021-07-26 10:11:22 +0200

    consistent naming + reorder alphabetically

---
+++ .config/nvim/lua/pluginConfig/nvimWebDevicons.lua
@@ -0,0 +1,14 @@
+require('nvim-web-devicons').setup {
+ override = {
+  pro = {
+    icon = "",
+    color = "#e37933",
+    name = "Prolog"
+  },
+  mom = {
+    icon = "",
+    color = "#6d8086",
+    name = "Groff_Mom"
+  }
+ }
+}
