9c3a3796fc511634320cb9b656063fcd5d43279c
Mahmoud Kassem <mail@itqan.de>
2021-08-13 20:52:28 +0200

    add treesitter playground + query linter

--- .config/nvim/lua/pluginConfig/treesitter.lua
+++ .config/nvim/lua/pluginConfig/treesitter.lua
@@ -1,5 +1,10 @@
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {"BufWrite", "CursorHold"},
   }
 }
