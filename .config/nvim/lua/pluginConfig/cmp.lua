9e14a3e9b7315d5ae67084023b9540b04a2aeaf0
Mahmoud Kassem <mail@itqan.de>
2021-08-31 09:27:25 +0200

    nvim-autopairs has been moved to it's own configuration file

--- .config/nvim/lua/pluginConfig/cmp.lua
+++ .config/nvim/lua/pluginConfig/cmp.lua
@@ -4,10 +4,3 @@ require('cmp').setup {
     { name = 'nvim_lua' }
   }
 }
-
-require('nvim-autopairs').setup {}
-require("nvim-autopairs.completion.cmp").setup {
-  map_cr = true,
-  map_complete = true,
-  autoselect = true
-}
