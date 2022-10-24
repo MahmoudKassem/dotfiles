021b89b3117c84582566da446d3412dc5e10e8ae
Mahmoud Kassem <mail@itqan.de>
2022-10-24 17:40:04 +0200

    update config for nvim_lsp completion source

--- .config/nvim/lua/pluginConfig/lsp.lua
+++ .config/nvim/lua/pluginConfig/lsp.lua
@@ -1,6 +1,6 @@
 local lsp_config = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 lsp_config.prolog_lsp.setup {
   capabilities = capabilities
