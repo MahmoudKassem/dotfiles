4680ece85074d0cf70cd8ed31332cc618e65953a
Mahmoud Kassem <mail@itqan.de>
2023-03-14 23:55:31 +0100

    delete obsolete vim-rooter config file

--- .config/nvim/lua/pluginConfig/vimRooter.lua
+++
@@ -1,4 +0,0 @@
-vim.g.rooter_change_directory_for_non_project_files = 'current'
-vim.g.rooter_patterns = {".git", "*.mod", "stack*.yaml"}
-vim.g.rooter_resolve_links = 1
-vim.g.rooter_silent_chdir = 1
