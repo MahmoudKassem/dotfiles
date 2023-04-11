17480879c094fcfa39ff39859bb94e9c116aee09
Mahmoud Kassem <mail@itqan.de>
2023-04-11 08:43:17 +0200

    close terminal window immediately

--- .config/nvim/lua/autoCommands.lua
+++ .config/nvim/lua/autoCommands.lua
@@ -4,5 +4,6 @@ end
 
 autoCommand('BufEnter *.pro set filetype=prolog')
 autoCommand('BufEnter *.mom set filetype=groff')
+autoCommand('TermClose * q')
 autoCommand('TermOpen * startinsert')
 autoCommand('TextYankPost * silent! lua vim.highlight.on_yank()')
