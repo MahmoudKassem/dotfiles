6578e9ce1b4242817579aa45dcd6a57b56a10b71
Mahmoud Kassem <mail@itqan.de>
2023-04-15 04:09:05 +0200

    add mappings to jump around in snippets

--- .config/nvim/lua/pluginConfig/cmp.lua
+++ .config/nvim/lua/pluginConfig/cmp.lua
@@ -15,7 +15,25 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true })
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-j>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        else
+            fallback()
+        end
+    end, { "i", "s" }),
+    ["<C-k>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end, { "i", "s" }),
   }),
   window = {
     completion = cmp.config.window.bordered(),
