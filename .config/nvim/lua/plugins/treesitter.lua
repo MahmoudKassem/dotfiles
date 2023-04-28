49d5a08ee211f689134a5f1f4fe2a8b1213114bf
Mahmoud Kassem <mail@itqan.de>
2023-04-28 10:11:03 +0200

    replace " by '

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
                 query_linter = {
                     enable = true,
                     use_virtual_text = true,
-                    lint_events = {"BufWrite", "CursorHold"},
+                    lint_events = {'BufWrite', 'CursorHold'},
                 }
             }
         end
