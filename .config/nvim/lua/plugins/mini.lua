d7ab7b4c3fb08e360efebe607839af9a14f351e5
Mahmoud Kassem <mail@itqan.de>
2023-04-28 10:02:21 +0200

    load the actually used mini plugins individually

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,31 +1,58 @@
 return {
-    'echasnovski/mini.nvim',
-    event = 'VeryLazy',
-    config = function ()
-        require('mini.comment').setup()
-        require('mini.move').setup({
-            mappings = {
-                left = '<leader>H',
-                right = '<leader>L',
-                down = '<leader>J',
-                up = '<leader>K',
-                line_left = '<leader>H',
-                line_right = '<leader>L',
-                line_down = '<leader>J',
-                line_up = '<leader>K'
-            }
-        })
-        require('mini.pairs').setup()
-        require('mini.surround').setup({
-            mappings = {
-                add = 'as',
-                delete = 'ds',
-                find = nil,
-                find_left = nil,
-                highlight = nil,
-                replace = 'cs',
-                update_n_lines = nil
-            }
-        })
-    end
+    {
+        'echasnovski/mini.comment',
+        keys = 'gc',
+        config = function ()
+            require('mini.comment').setup()
+        end
+    },
+    {
+        'echasnovski/mini.jump',
+        keys = {'f', 'F', 't', 'T'},
+        config = function ()
+            require('mini.jump').setup()
+        end
+    },
+    {
+        'echasnovski/mini.move',
+        keys = {'<leader>H', '<leader>L', '<leader>J', '<leader>K'},
+        config = function ()
+            require('mini.move').setup({
+                mappings = {
+                    left = '<leader>H',
+                    right = '<leader>L',
+                    down = '<leader>J',
+                    up = '<leader>K',
+                    line_left = '<leader>H',
+                    line_right = '<leader>L',
+                    line_down = '<leader>J',
+                    line_up = '<leader>K'
+                }
+            })
+        end
+    },
+    {
+        'echasnovski/mini.pairs',
+        event = 'InsertEnter',
+        config = function ()
+            require('mini.pairs').setup()
+        end
+    },
+    {
+        'echasnovski/mini.surround',
+        keys = {'ys', 'ds', 'cs'},
+        config = function ()
+            require('mini.surround').setup({
+                mappings = {
+                    add = 'as',
+                    delete = 'ds',
+                    replace = 'cs',
+                    find = nil,
+                    find_left = nil,
+                    highlight = nil,
+                    update_n_lines = nil
+                }
+            })
+        end
+    }
 }
