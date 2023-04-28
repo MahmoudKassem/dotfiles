1db95a9a84190bbba460bf0f64e0c3d96243db6b
Mahmoud Kassem <mail@itqan.de>
2023-04-28 10:05:49 +0200

    replace " by '

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -2,7 +2,7 @@ return {
     'ahmedkhalf/project.nvim',
     lazy = true,
     config = function()
-        require("project_nvim").setup({
+        require('project_nvim').setup({
             show_hidden = true
         })
     end
