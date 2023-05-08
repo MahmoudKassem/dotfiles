bc34278aa0139976472aba23670025f775485432
Mahmoud Kassem <mail@itqan.de>
2023-05-08 06:50:09 +0200

    tree inspection capabilites and the query linter are now in core

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,13 @@
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        ft = {'go', 'haskell', 'lua', 'sh', 'query', 'yaml'},
-        config = function()
-            require('nvim-treesitter.configs').setup {
-                ensure_installed = {'go', 'haskell', 'lua', 'bash', 'query', 'yaml'},
-                highlight = {
-                    enable = true
-                },
-                query_linter = {
-                    enable = true,
-                    use_virtual_text = true,
-                    lint_events = {'BufWrite', 'CursorHold'},
-                }
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    ft = {'go', 'haskell', 'lua', 'sh', 'query', 'yaml'},
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = {'go', 'haskell', 'lua', 'bash', 'query', 'yaml'},
+            highlight = {
+                enable = true
             }
-        end
-    },
-    {
-        'nvim-treesitter/playground',
-        cmd = 'TSPlaygroundToggle',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter'
         }
-    }
+    end
 }
