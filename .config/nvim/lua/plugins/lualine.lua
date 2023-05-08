0e8e5c5e282fec533430de83c65593170774fea9
Mahmoud Kassem <mail@itqan.de>
2023-05-08 06:45:46 +0200

    add branch icon in statusline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -52,7 +52,8 @@ return {
                 },
                 lualine_b = {
                     {
-                        'b:gitsigns_head'
+                        'b:gitsigns_head',
+                        icon = ''
                     },
                     diff
                 },
