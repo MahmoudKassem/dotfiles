1175807a55aeaf8b349eb0fa9ebf908b7b15b7dd
Mahmoud Kassem <mail@itqan.de>
2023-05-12 08:11:10 +0200

    update icons for statusbar scripts

--- .local/bin/get-screen-brightness.sh
+++ .local/bin/get-screen-brightness.sh
@@ -4,9 +4,9 @@ currentBrightness=$((
   $(head /sys/class/backlight/amdgpu_bl0/brightness)*100 /
   $(head /sys/class/backlight/amdgpu_bl0/max_brightness)))
 if [ $currentBrightness -gt 70 ]
-  then icon=
+  then icon=󰃠
 elif [ $currentBrightness -lt 30 ]
-  then icon=
-  else icon=
+  then icon=󰃞
+  else icon=󰃟
 fi
 printf " %s %s%% \\n" $icon $currentBrightness
--- .local/bin/get-volume.sh
+++ .local/bin/get-volume.sh
@@ -3,11 +3,11 @@
 masterInfo="amixer -M get Master"
 pcmInfo="amixer -M get PCM"
 currentVolume=$($pcmInfo | sed -n -E 's/^.*Front Left:.*\[([0-9]+)%\].*$/\1/p')
-$masterInfo | grep -q off && printf "婢 $currentVolume%% \\n" && exit
+$masterInfo | grep -q off && printf "󰸈 $currentVolume%% \\n" && exit
 if [ "$currentVolume" -gt 70 ]
-  then icon=墳
+  then icon=󰕾
 elif [ "$currentVolume" -lt 30 ]
-  then icon=奄
-  else icon=奔
+  then icon=󰕿
+  else icon=󰖀
 fi
 printf " %s %s%% \\n" $icon "$currentVolume"
