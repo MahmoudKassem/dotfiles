2152524b0bc8503b9f618f0f8061c5f1df844f9d
Mahmoud Kassem <mail@itqan.de>
2024-07-05 23:20:14 +0200

    use treesitter for prolog

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    ft = {'go', 'haskell', 'lua', 'sh', 'query', 'yaml'},
+    ft = {'go', 'haskell', 'lua', 'sh', 'query', 'yaml', 'prolog'},
     config = function()
         require('nvim-treesitter.configs').setup {
-            ensure_installed = {'go', 'haskell', 'lua', 'bash', 'query', 'yaml'},
+            ensure_installed = {'go', 'haskell', 'lua', 'bash', 'query', 'yaml', 'prolog'},
             highlight = {
                 enable = true
             }
