8f9102f37ef638eb8e5432166164aeb201f74c5b
Mahmoud Kassem <mail@itqan.de>
2025-08-23 22:51:22 +0200

    adjust brightness values to match the new range

--- .local/bin/change-screen-brightness.sh
+++ .local/bin/change-screen-brightness.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-currentBrightness=$(cat /sys/class/backlight/amdgpu_bl1/brightness)
+currentBrightness=$(head /sys/class/backlight/amdgpu_bl1/brightness)
+maxBrigthness=$(head /sys/class/backlight/amdgpu_bl1/max_brightness)
 newBrightness=$(( currentBrightness + $1 ))
-if [ "$newBrightness" -ge 0 ] && [ "$newBrightness" -lt 255 ]
+if [ "$newBrightness" -ge 0 ] && [ "$newBrightness" -le "$maxBrigthness" ]
   then printf "%s" $newBrightness | doas -- tee -a /sys/class/backlight/amdgpu_bl1/brightness > /dev/null
-  else notify-send "brightness values range from 0 to 255 current value is $currentBrightness"
+  else notify-send "brightness values range from 0 to $maxBrigthness current value is $currentBrightness"
 fi
