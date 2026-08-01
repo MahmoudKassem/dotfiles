a36e563c022ff6144c083649ab503e5bfecaaa3a
Mahmoud Kassem <mail@itqan.de>
2026-08-02 00:20:41 +0200

    use kawkab mono font for arabic

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,19 +1,26 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local current_scheme = wezterm.color.get_builtin_schemes()["GruvboxDarkHard"]
 
 config.bidi_enabled = true
-config.bidi_direction = "AutoLeftToRight"
-config.color_scheme = 'GruvboxDarkHard'
-config.enable_tab_bar = false
-config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 13
-config.font_rules = {
-	{
-		intensity = "Bold",
-		italic = false,
-		font = wezterm.font("", { weight = "Regular" })
-	}
+config.bold_brightens_ansi_colors = "No"
+config.colors = {
+  scrollbar_thumb = current_scheme.ansi[8]
 }
+config.color_scheme = "GruvboxDarkHard"
+config.enable_tab_bar = false
+config.enable_scroll_bar = true
+config.font = wezterm.font_with_fallback {
+  { family = "HackNerdFont Mono",
+    weight = "Regular"
+  },
+  {
+    family = "Kawkab Mono",
+    weight = "Bold"
+  }
+}
+config.font_size = 13.0
+config.min_scroll_bar_height = "4cell"
 config.window_background_opacity = 0.92
 config.window_close_confirmation = "NeverPrompt"
 
